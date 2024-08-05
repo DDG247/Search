f57f721e9081fe68773237f432ee8068bc7dd667
DDG247 <xiaogouren12@163.com>
2024-08-05 10:33:21 +0800

    text

---
+++ src/SequentialSearch.java
@@ -0,0 +1,9 @@
+public class SequentialSearch {
+    public static void main(String[] args) {
+
+    }
+    public static int sequentialSearch(int[] arr, int target) {
+
+        return target;
+    }
+}
